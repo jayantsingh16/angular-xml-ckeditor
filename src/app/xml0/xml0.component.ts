5ccc530e9ad580710e26288c90e3d4ce2e4b6f91
jayantsingh16 <jayant.singh@nl.abnamro.com>
2021-05-25 08:15:27 +0200

    3 files modified

--- src/app/xml0/xml0.component.ts
+++ src/app/xml0/xml0.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import xml2js from 'xml2js';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { saveAs } from 'file-saver';
+import ClassicEditorWithAutosave from '@ckeditor/ckeditor5-build-classic';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -9,43 +9,22 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./xml0.component.css']
 })
 export class Xml0Component {
-  public xmlItems: any;
-  public Editor = ClassicEditor;
-  public editorData: any;
-  constructor(private _http: HttpClient) { this.loadXML(); }
-  loadXML() {
-    this._http.get('/assets/users.xml',
-      {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'text/xml')
-          .append('Access-Control-Allow-Methods', 'GET')
-          .append('Access-Control-Allow-Origin', '*')
-          .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),
-        responseType: 'text'
-      })
-      .subscribe((data) => {
-        this.parseXML(data)
-          .then((data) => {
-            this.xmlItems = data;
-          });
-      });
+  public Editor = ClassicEditorWithAutosave;
+  public editorData: any
+  public config = {
+		autosave: {
+			waitingTime: 5000,
+			save: Editor => this.saveData( Editor.getData() )
+		},
+	}
+  saveFile(data) {
+    const blob = 
+        new Blob([
+                 data], 
+                 {type: "text/xml;charset=utf-8"});
+    saveAs(blob, "/assets/users.xml");
   }
-  
-  parseXML(data) {
-    return new Promise(resolve => {
-      var k: string | number,
-        arr = [],
-        parser = new xml2js.Parser(
-          {
-            trim: true,
-            explicitArray: true
-          });
-      parser.parseString(data, function (err, result) {
-        var obj = result.Employee;
-        const serializer = new XMLSerializer();
-        const xmlStr = serializer.serializeToString(obj);
-        this.editorData = xmlStr;
-      });
-    });
-  }
+	public saveData(data) {
+    this.saveFile(data);
+	}
 }
